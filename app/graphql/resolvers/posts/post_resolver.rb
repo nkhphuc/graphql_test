57928095f7e8ae031efd41680e82b3926aa89911
Nguyen Khoa Hoang Phuc <nkhphuc@gmail.com>
2023-11-02 17:30:02 +0700

    Query a single post.

--- app/graphql/resolvers/posts/post_resolver.rb
+++ app/graphql/resolvers/posts/post_resolver.rb
@@ -1,6 +1,14 @@
 module Resolvers
   module Posts
-    class PostResolver < Resolver::BaseResolver
+    class PostResolver < BaseResolver
+      type Types::Objects::Posts::PostType, null: true
+      description "Query a single post"
+
+      argument :id, ID, required: true
+
+      def resolve(id:)
+        Post.find_by(id:)
+      end
     end
   end
 end
