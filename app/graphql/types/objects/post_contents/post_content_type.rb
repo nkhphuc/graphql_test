39c777c7bc4a4a82bc722350c26d808a594a9b59
Nguyen Khoa Hoang Phuc <nkhphuc@gmail.com>
2023-11-03 16:57:24 +0700

    Post content type.

---
+++ app/graphql/types/objects/post_contents/post_content_type.rb
@@ -0,0 +1,14 @@
+module Types
+  module Objects
+    module PostContents
+      class PostContentType < Types::BaseObject
+        field :id, ID, null: false
+        field :part, Int, null: true
+        field :body, String, null: false
+        field :post_id, Int, null: false
+        field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+        field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+      end
+    end
+  end
+end
