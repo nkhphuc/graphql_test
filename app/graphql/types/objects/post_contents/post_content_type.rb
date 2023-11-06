b45dec9f001e52b843615ecb9da82fa4f95377aa
Nguyen Khoa Hoang Phuc <nkhphuc@gmail.com>
2023-11-06 08:50:55 +0700

    Add post to post content queries.

--- app/graphql/types/objects/post_contents/post_content_type.rb
+++ app/graphql/types/objects/post_contents/post_content_type.rb
@@ -6,6 +6,7 @@ module Types
         field :part, Int, null: true
         field :body, String, null: false
         field :post_id, Int, null: false
+        field :post, Types::Objects::Posts::PostType, null: false
         field :created_at, GraphQL::Types::ISO8601DateTime, null: false
         field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
       end
