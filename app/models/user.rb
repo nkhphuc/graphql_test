67dd64c7545e878b44c704647be93cee94824336
Nguyen Khoa Hoang Phuc <nkhphuc@gmail.com>
2023-11-06 17:39:11 +0700

    Add trackable devise module.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :validatable
-    #, :confirmable, :omniauthable, :trackable
+    :recoverable, :rememberable, :validatable, :trackable
+    #, :confirmable, :omniauthable
   include GraphqlDevise::Authenticatable
 end
